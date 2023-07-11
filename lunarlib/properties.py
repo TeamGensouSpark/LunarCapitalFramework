50f9cdb0a6c5c0ba32a14fa89ac44c40847d23f6
H2Sxxa <H2Sxxa0w0@gmail.com>
2023-07-11 10:59:06 +0800

    use pdm to run project

--- lunarlib/properties.py
+++ lunarlib/properties.py
@@ -1,6 +1,7 @@
-from Remilia.lite.LiteResource import File
+from Remilia.res import rFile
 
 from .utils import uformat
+
 
 class GradleProperties:
     def __init__(self,path) -> None:
@@ -9,7 +10,7 @@ class GradleProperties:
     @property
     def properties(self):
         properties_group={}
-        for line in [line for line in [line for line in File(self.path).text.splitlines() if len(line)>0] if line[0] != '#']:
+        for line in [line for line in [line for line in rFile(self.path).text.splitlines() if len(line)>0] if line[0] != '#']:
             slices=line.split("=")
             name=uformat.extract_escape(slices[0])
             slices.pop(0)
