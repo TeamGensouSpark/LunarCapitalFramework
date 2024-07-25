ac2fdfbdc0574e4a370791c323c0011d249a0b8e
H2Sxxa <h2sxxa0w0@gmail.com>
2024-07-25 10:03:22 +0800

    Maven repo Upgrade ~~It should work I think.~~

--- setup-cli.ps1
+++ setup-cli.ps1
@@ -1,7 +1,7 @@
 cd script/lunar-cli
 python -m pip install --upgrade pip
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pdm
-pdm config pypi.url https://pypi.tuna.tsinghua.edu.cn/simple
+pip install -i https://mirror.sjtu.edu.cn/pypi/web/simple pdm
+pdm config pypi.url https://mirror.sjtu.edu.cn/pypi/web/simple
 python -m pdm self update
 pdm install
 cd ../..
